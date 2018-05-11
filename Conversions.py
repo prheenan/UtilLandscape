6defee687d63b14e8022a7754fd80e2faeb33370
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2018-05-11 08:58:13 -0600

    added better commenting

--- Conversions.py
+++ Conversions.py
@@ -11,4 +11,12 @@ import sys
 from scipy.optimize import fminbound,newton,brentq
 
 def kcal_per_mol_per_J():
+    """
+    :return: the conversion from J/molecule to kcal/mol
+    """
+    # 1 kcal per mol = 4.184 kJ/mol
+    # 1 kcal per mol = (4.184e3 J / molecule) * (1/Avogadro)
+    #                ~ 6.947e-21 J / molecule
+    # in other words, to convert from J/molecule to kcal/mol, multiply by
+    # ~(1 kcal/mol)/(6.947e-21 J / molecule)
     return 1/6.9477e-21
